6f74988f952e022967f83f14f95c22b8ef2f2c1d
Charles-Antoine Giroux <charles-antoine.giroux.2@ulaval.ca>
2021-01-27 14:11:56 -0500

    recherche

---
+++ script.js
@@ -0,0 +1,82 @@
+/* document.getElementById("userentry").addEventListener("change", adjustSelectItems); */
+
+function adjustSelectItems(){
+    let selectItems = getAllItemsInSelect();
+    let filter = getUserEntry();
+    filterSelectItems(selectItems, filter);
+}
+
+function getAllItemsInSelect(){
+    var selectBox = document.getElementById("col");
+    return selectBox.options;
+}
+
+function getUserEntry() {
+    var userEntry = document.getElementById("userentry");
+    return userEntry.value;
+}
+
+function filterSelectItems(selectItems, filter){
+    var matchesFound = 0;
+    for(var i = 0; i < selectItems.length; ++i){
+        if (selectItems[i].id === "nothingfound"){
+            continue;
+        }
+        if(!selectItems[i].value.toLowerCase().includes(filter.toLowerCase())) {
+            selectItems[i].hidden = true;
+        }
+        else{
+            selectItems[i].hidden = false;
+            console.log("Matching item found: ", selectItems[i].value)
+            matchesFound++;
+        }
+    }
+    setSelectDropDownToCorrectSize(matchesFound);
+    setValidSelectedItem(selectItems, filter);
+    setNoValueFoundVisibility(matchesFound);
+}
+
+function setValidSelectedItem(selectItems, filter) {
+    var index = findFirstMatch(selectItems, filter);
+    if (index !== -1){
+        selectItems[index].selected = "selected";
+    }
+    else{
+        document.getElementById("nothingfound").selected = "selected";
+    }
+}
+
+
+function setSelectDropDownToCorrectSize(itemsQte){
+    var selectBox = document.getElementById("col");
+    if (itemsQte > 0){
+        selectBox.size = itemsQte;
+
+    }
+    else{
+        selectBox.size = 1;
+    }
+}
+
+function findFirstMatch(selectItems, filter) {
+    for (var i = 0; i < selectItems.length; ++i) {
+        if (selectItems[i].id === "nothingfound") {
+            continue;
+        }
+        if (selectItems[i].value.toLowerCase().includes(filter.toLowerCase())){
+            return i;
+        }
+    }
+    return -1;
+}
+
+function setNoValueFoundVisibility(matchesFound) {
+    var nothingFoundOption = document.getElementById("nothingfound");
+    var selectBox = document.getElementById("col");
+    if (matchesFound === 0) {
+        nothingFoundOption.hidden = false;
+    }
+    else{
+        nothingFoundOption.hidden = true;
+    }
+}
